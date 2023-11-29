45ee8b867ebb71590cd548730019837389256567
Brandon Mord <bdrmord002@gmail.com>
2023-11-28 23:13:44 -0600

    Baseline and initial addition of project derived from florinpop17 app-ideas repo

---
+++ Demonstrations/View/Bin2Dec.xaml.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Demonstrations.View
+{
+    /// <summary>
+    /// Interaction logic for Bin2Dec.xaml
+    /// </summary>
+    public partial class Bin2Dec : UserControl, INotifyPropertyChanged
+    {
+        private enum BinStringErrors
+        {
+            eNone,
+            eInvalidNumber,
+            eInvalidLength
+        }
+
+        private string _result;
+        private string _input;
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        public Bin2Dec()
+        {
+            InitializeComponent();
+            DataContext = this;
+        }
+
+        public string Result { 
+            get => _result;
+            set
+            {
+                _result = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string Input { 
+            get => _input;
+            set 
+            {
+                var res = IsValidInput(value);
+                switch(res)
+                {
+                    case BinStringErrors.eInvalidNumber:
+                    case BinStringErrors.eInvalidLength:
+                        MessageBox.Show(res==BinStringErrors.eInvalidLength ? "Entry must be no larger than 8 characters" : 
+                            "Entry must be 0 or 1", "WARNING", MessageBoxButton.OK, MessageBoxImage.Error);
+                        break;
+                    case BinStringErrors.eNone:
+                    default:
+                        _input = value;
+                        break;
+                }
+                Result = string.Empty;
+                OnPropertyChanged();
+            }
+        }
+
+        // Create the OnPropertyChanged method to raise the event
+        // The calling member's name will be used as the parameter.
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+
+        private void btnGenerate_Click(object sender, RoutedEventArgs e)
+        {
+            int pow = 0;
+            int res = 0;
+            for(int i = Input.Length-1; i >= 0; i--)
+            {
+                if (Input[i] == '1')
+                    res += (int)Math.Pow(2, pow);
+                pow++;
+                Result = res.ToString();
+            }
+        }
+
+        private BinStringErrors IsValidInput(string input)
+        {
+            Regex rx = new Regex("^[01]{0,8}$");
+            if (rx.IsMatch(input))
+                return BinStringErrors.eNone;
+            if(input.Length>8)
+                return BinStringErrors.eInvalidLength;
+            return BinStringErrors.eInvalidNumber;
+        }
+    }
+}
---
+++ Demonstrations/View/Calculator.xaml.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Demonstrations.View
+{
+    /// <summary>
+    /// Interaction logic for Calculator.xaml
+    /// </summary>
+    public partial class Calculator : UserControl, INotifyPropertyChanged
+    {
+        private enum Operation
+        {
+            eInvalid,
+            eEq,
+            eMinus,
+            ePLus,
+            eMultiply,
+            eDivide,
+            eClear,
+            eClearAll,
+            eSign
+        }
+
+        private double _result;
+        private double _input;
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        // Create the OnPropertyChanged method to raise the event
+        // The calling member's name will be used as the parameter.
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+        public double Result { get => _result; set { _result = value; OnPropertyChanged(); } }
+
+        public double Input 
+        { 
+            get => _input; 
+            set 
+            { 
+                if(IsValidInput(value))
+                    _input = value; 
+                OnPropertyChanged(); 
+            } 
+        }
+
+        private bool IsValidInput(double value)
+        {
+            Regex rx = new Regex("^(d+.?d+){0,8}$");
+            return rx.IsMatch(value.ToString());
+        }
+
+        public Calculator()
+        {
+            InitializeComponent();
+        }
+
+        private void OpBtn(Operation op)
+        {
+            switch(op)
+            {
+                case Operation.eEq:
+                    break;
+                case Operation.eSign:
+                    break;
+                case Operation.eMinus:
+                    break;
+                case Operation.ePLus:
+                    break;
+                case Operation.eMultiply:
+                    break;
+                case Operation.eDivide:
+                    break;
+                case Operation.eClear:
+                    break;
+                case Operation.eClearAll:
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void BtnMediator(object sender, RoutedEventArgs e)
+        {
+            var tag = ((Button)sender).Tag.ToString();
+            if (tag.Contains("Op:"))
+            {
+                var toks = tag.Split(':');
+                Operation result;
+                if(Enum.TryParse<Operation>(toks[1], out result))
+                {
+                    OpBtn(result);
+                }
+                
+            }
+            else if(tag.Contains("Decimal"))
+            {
+
+            }
+            else
+            {
+                Result *= 10;
+                Result += int.Parse(tag);
+            }
+        }
+    }
+}
